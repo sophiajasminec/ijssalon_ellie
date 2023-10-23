09400258f224e0eca3af777a2d7ddb61017beb3d
sophiajasminec <sophiajasmine20@gmail.com>
2023-10-23 14:50:00 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,16 @@
+mijn_dictonary = "prijzen"
+mijn_dictonary = {
+    "aardbei" : "3",
+    "vanille" : "4",
+    "chocolade" : "5"
+}
+print (mijn_dictonary)
+
+aanbieding = (3 * 0.8)
+reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+
+reclame_tekst2 = reclame_tekst [:63]
+reclame_tekst3 = reclame_tekst.upper() [:63]
+reclame_tekst4 = reclame_tekst3 
+for el in reclame_tekst4:
+    print(reclame_tekst4.lower())
